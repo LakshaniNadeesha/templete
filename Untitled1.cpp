f7b84be292b692be9300375016cd45a816d58f68
LakshaniNadeesha <83568940+LakshaniNadeesha@users.noreply.github.com>
2021-05-13 22:49:56 +0530

    Add a c++ file

---
+++ Untitled1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+template <class T, class U, class V = double>
+
+class A{
+	T x;
+	U y;
+	V z;
+	static int count;
+};
+
+
+int main(){
+	A<int, int>a;
+	A<double, double>b;
+	cout << sizeof(a) << ", ";
+	cout << sizeof(b) << endl;
+	
+	
+	return 0;
+	
+}
